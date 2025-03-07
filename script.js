29358d63d80bcb23133a913b5bb7eea6ef7f8ca9
Alejandro Rezzano <rezzanoalejandro@gmail.com>
2025-03-07 12:33:35 -0300

    Add ID to book obj and create function to create and push new books into myLibrary array

--- script.js
+++ script.js
@@ -5,4 +5,18 @@ function Book(title,author,pages,beenRead){
     this.author = author,
     this.pages = pages,
     this.beenRead = beenRead
+    this.id = crypto.randomUUID();
+    this.readMsg;
+    if(beenRead){
+        this.readMsg = " has been read already.";
+    }else{
+        this.readMsg = " hasn't been read yet.";
+    }
+    this.message = `${this.title} was written by ${this.author}, has ${this.pages} pages and ${this.readMsg} This book's ID is ${this.id}`;
 };
+
+function addBookToLibrary(title,author,pages,beenRead){
+    let book = new Book(title,author,pages,beenRead);
+
+    myLibrary.push(book);
+}
